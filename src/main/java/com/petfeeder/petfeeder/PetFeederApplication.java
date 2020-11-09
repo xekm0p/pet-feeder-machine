59399ac73e9d0d53174d4a369e805d71a337c34e
xekm0p <alexander.tonev97@gmail.com>
2020-11-09 21:06:40 +0200

    First project load.

---
+++ src/main/java/com/petfeeder/petfeeder/PetFeederApplication.java
@@ -0,0 +1,13 @@
+package com.petfeeder.petfeeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFeederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFeederApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petfeeder/petfeeder/PetFeederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petfeeder.petfeeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFeederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
