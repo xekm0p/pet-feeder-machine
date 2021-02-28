52ae452ae316e84f41b6ccc0c83387429d4ceb9e
xekm0p <alexander.tonev97@gmail.com>
2021-02-28 16:42:46 +0200

    adding certificates

--- src/main/java/com/petfeeder/petfeeder/config/AppConfig.java
+++ src/main/java/com/petfeeder/petfeeder/config/AppConfig.java
@@ -1,5 +1,11 @@
 package com.petfeeder.petfeeder.config;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.connection.CachingConnectionFactory;
@@ -18,4 +24,31 @@ public class AppConfig {
         jmsTemplate.setPubSubDomain(true);
         return jmsTemplate;
     }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection securityCollection = new SecurityCollection();
+                securityCollection.addPattern("/*");
+                securityConstraint.addCollection(securityCollection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+        return tomcat;
+        }
+
+        private Connector httpToHttpsRedirectConnector () {
+            Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+            connector.setScheme("http");
+            connector.setPort(8082);
+            connector.setSecure(false);
+            connector.setRedirectPort(8443);
+            return connector;
+        }
 }
